24a33ec785dc6d4a231e5f335b099d315c7aa925
sm4x <max.schattauer@gmail.com>
2019-05-07 15:37:02 +0200

    Chapter 04

--- microblog.py
+++ microblog.py
@@ -1,4 +1,8 @@
-from app import app
+from app import app, db
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
 
 # Run from Main Program
 if __name__ == '__main__':
